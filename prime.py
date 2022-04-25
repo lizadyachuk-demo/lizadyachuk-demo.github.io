8989b008fdf677dbf9a286561409cf73eb27310f
lizadyachuk-demo <99094665+lizadyachuk-demo@users.noreply.github.com>
2022-04-25 09:16:10 +0300

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,10 +1,13 @@
 def is_prime(n):
-    if n % 2 == 0 or n % 3 == 0 or n % 4 == 0 or n % 5 == 0 or n % 6 == 0 or n % 7 == 0 or n % 8 == 0 or n % 9 == 0:
+    if n == 2 or n == 3 or n == 5 or n == 7:
+        return True
+    elif n % 2 == 0 or n % 3 == 0 or n % 4 == 0 or n % 5 == 0 or n % 6 == 0 or n % 7 == 0 or n % 8 == 0 or n % 9 == 0:
         return False
     elif n == 1:
         return False
     else:
         return True
 
-print(is_prime(17))
+print(is_prime(33))
+
 
